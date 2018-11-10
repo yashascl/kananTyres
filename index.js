da88aee78254a93523686e5b372d05fad343ea30
yashascl <yashas.cl@gmail.com>
2018-11-10 13:32:25 +0530

    changed the port name in package file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require("path");
 
-app.set('port',(process.env.port || 5000));
+app.set('port',(process.env.PORT || 5000));
 
 app.use(express.static(__dirname+'/public'));
 
